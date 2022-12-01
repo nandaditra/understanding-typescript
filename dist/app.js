7c3bbcbb0561cab7fa1da2f3d03376baccca4971
perpustakaan pusat <nandaditra56@gmail.com>
2022-12-01 15:23:07 +0700

    change some component file

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,13 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Max',
-    age: 30,
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name);
-    }
+const p2 = {
+    name: "Adam Girvil",
+    privileges: ["create-server"],
+    startDate: new Date()
 };
+function AddComponent(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
 //# sourceMappingURL=app.js.map
---
+++ dist/interface.js
@@ -0,0 +1,10 @@
+"use strict";
+let user1;
+user1 = {
+    name: 'Max',
+    age: 30,
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
+    }
+};
+//# sourceMappingURL=interface.js.map
